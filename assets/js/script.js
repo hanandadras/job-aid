cf8b4b5e811c7d620d026fc12917af87e675f810
Hector R <hectorommmo@gmail.com>
2021-11-19 20:02:33 -0800

    Loading responses on Search Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var formEl = document.querySelector("#job-form");
+var searchEl = document.querySelector("#recent-searches")
 var searchHistory = [];
 
-function getAPI(title) {
-    fetch("https://job-search4.p.rapidapi.com/monster/search?query=" + title + "&state=CA&page=1", {
+function getAPI(title, zip) {
+    fetch("https://job-search4.p.rapidapi.com/linkedin/search?query=" + title + "&page=1", {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "job-search4.p.rapidapi.com",
@@ -12,28 +13,67 @@ function getAPI(title) {
     .then(resp => resp.json())
     .then(response => {
         console.log(response);
+
+        fetch("https://us-zip-code-information.p.rapidapi.com/?zipcode=" + zip, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "us-zip-code-information.p.rapidapi.com",
+                "x-rapidapi-key": "7d3981e8bemsh702d01ad1635c18p10dd1fjsn83e5763a1078"
+            }
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+
+            // var x = response.jobs.length;
+
+            // console.log("Size of response: " + x);
+
+            // var y = JSON.stringify(data[0].AreaCode);
+
+            // console.log("Area Code: " + y);
+
+            for(var i = 0; i < response.jobs.length; i++) {
+
+                // create div to hold task info and add to list item
+                var searchInfoEl = document.createElement("div");
+            
+                // add HTML content to div
+                searchInfoEl.innerHTML = "<div class='uk-card uk-card-default uk-card-body'><h3 class='uk-card-title'>" + response.jobs[i].title + "</h3>" + 
+                "<a href='" + response.jobs[i].detail_url + "' target='_blank'>Job Posting</a><p>" + response.jobs[i].description + "</p><p>" + response.jobs[i].source + "</p></div>";
+    
+                searchEl.appendChild(searchInfoEl);
+            }
+
+
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+
     })
     .catch(err => {
         console.error(err);
     });
 }
 
-function getZIP(zip) {
-    fetch("https://us-zip-code-information.p.rapidapi.com/?zipcode=" + zip, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "us-zip-code-information.p.rapidapi.com",
-            "x-rapidapi-key": "7d3981e8bemsh702d01ad1635c18p10dd1fjsn83e5763a1078"
-        }
-    })
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.error(err);
-    });   
-}
+// function getZIP(zip) {
+//     fetch("https://us-zip-code-information.p.rapidapi.com/?zipcode=" + zip, {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "us-zip-code-information.p.rapidapi.com",
+//             "x-rapidapi-key": "7d3981e8bemsh702d01ad1635c18p10dd1fjsn83e5763a1078"
+//         }
+//     })
+//     .then(resp => resp.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });   
+// }
 
 var formHandler = function(event) {
     event.preventDefault();
@@ -41,8 +81,8 @@ var formHandler = function(event) {
     titleInput = document.querySelector("input[name='job-title']").value;
     zipInput = document.querySelector("input[name='zip-code']").value;
 
-    getAPI(titleInput);
-    getZIP(zipInput);
+    getAPI(titleInput, zipInput);
+    // getZIP(zipInput);
 
     var searchObj = {
         title: titleInput,
